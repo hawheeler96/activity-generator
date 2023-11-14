d309d033a4e4c923ff4789c21bd4f5dea160ff5d
Holly Wheeler <hollyawheeler.96@gmail.com>
2023-11-14 12:48:55 -0800

    add form and random generator functionality to index.js, add formatting to index.html, beginning of style sheet in style.css

--- index.js
+++ index.js
@@ -1,2 +1,65 @@
 //global consts, etc. 
-const API = "https://my-json-server.typicode.com/hawheeler96/activity-generator-api"
+const API = "http://localhost:3000/activities"
+let activitiesArr = []
+const activitiesCollection = document.getElementById('activity-collection');
+document.getElementById('new-activity-button').addEventListener('click', () =>renderActivities(activitiesArr));
+document.getElementById('add-activity-form').addEventListener('submit', newActivity)
+
+
+fetch(API)
+.then(res => res.json())
+.then(activities => {
+    activitiesArr = activities 
+    renderActivities(activities)
+})
+
+
+function renderActivities(activities) {
+    activitiesCollection.innerHTML = ""
+    let randomNumber = Math.floor(Math.random () * activitiesArr.length)
+    createCard(activities[randomNumber])
+}
+
+function createCard(activity) {
+    activitiesCollection.classList = 'card'
+    activitiesCollection.innerHTML = `
+    <h2>${activity.name}<h2>
+        <img class = 'img' src='${activity.image}' />
+        <p>${activity.description}</p>
+    `
+}
+
+function newActivity(e) {
+    e.preventDefault()
+    const newActivityItem = {
+        name: e.target.name.value,
+        image: e.target.image.value,
+        description: e.target.description.value
+    }
+    if (newActivityItem.name === "") {
+        alert('Please add activity name!')
+    }
+    else if (newActivityItem.image === "") {
+        alert('Please add activity image!')
+    }
+    else if (newActivityItem.description === "") {
+        alert('Please add activity description!')
+    }
+    else {
+        fetch(API, {
+            method: 'POST',
+            headers:
+            {
+              "Content-Type": "application/json",
+              Accept: "application/json"
+            },
+            body: JSON.stringify(newActivityItem)
+        })
+        .then(res => res.json())
+        .then(data => {
+            activitiesArr.push(data)
+            alert('Activity added!')
+        })
+        e.target.reset()
+    }
+}
