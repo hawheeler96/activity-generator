98a381932345e11a0b6288981046f120656102c2
Holly Wheeler <hollyawheeler.96@gmail.com>
2023-11-14 15:17:50 -0800

    add user recommendations and recommendation button

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 //global consts, etc. 
 const API = "http://localhost:3000/activities"
 let activitiesArr = []
+let currentFavoriteArr = []
+let allFavoritesArr = []
 const activitiesCollection = document.getElementById('activity-collection');
+const favoritesCollection = document.getElementById('favorites-list');
 document.getElementById('new-activity-button').addEventListener('click', () =>renderActivities(activitiesArr));
 document.getElementById('add-activity-form').addEventListener('submit', newActivity)
 
 
+//pull activities from db and render random one onto page
 fetch(API)
 .then(res => res.json())
 .then(activities => {
@@ -23,12 +27,52 @@ function renderActivities(activities) {
 function createCard(activity) {
     activitiesCollection.classList = 'card'
     activitiesCollection.innerHTML = `
-    <h2>${activity.name}<h2>
+        <h2>${activity.name}<h2>
         <img class = 'img' src='${activity.image}' />
         <p class = 'small-text'>${activity.description}</p>
+        <button id = 'activity-${activity.id}'>Reccomend &#x2764;</button>
     `
+    const recommendationBtn = document.getElementById(`activity-${activity.id}`)
+    recommendationBtn.addEventListener('click', () => handleRecommendations(activity))
 }
 
+//user recommendation list
+fetch(API)
+.then(res => res.json())
+.then(activities => {
+    renderUserRecommendations(activities)
+})
+
+function renderUserRecommendations(activities) {
+    activities.forEach(createRecommendation)
+}
+
+function createRecommendation(activities) {
+    const newRecommendation = document.createElement('div');
+    newRecommendation.textContent = `${activities.recommendations} users recommend ${activities.name}`
+    if (activities.recommendations > 0) {
+        favoritesCollection.append(newRecommendation);
+    }
+}
+
+function handleRecommendations (activity) {
+    let recommendations = activity.recommendations + 1
+    fetch (`${API}/${activity.id}`, {
+        method: 'PATCH',
+        headers: 
+        {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: JSON.stringify({recommendations})
+    })
+    .then (res => res.json())
+    .then (() => {
+        activity.recommmendations = recommendations
+    })
+}
+
+//adds new activity to database
 function newActivity(e) {
     e.preventDefault()
     const newActivityItem = {
